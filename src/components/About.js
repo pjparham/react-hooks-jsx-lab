4c26e41f7aee2e88c2dfe832d6305aad5c030823
Parker Parham <parkerparham@gmail.com>
2022-08-30 15:07:26 -0400

    Done with assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p> Integer convallis risus ex, sit amet sagittis diam pretium nec. Aenean facilisis ut urna vitae molestie. Etiam ut ornare nunc. Morbi et ex in felis condimentum condimentum. Sed magna enim, volutpat ut lacus sit amet, pretium lacinia mi. In scelerisque diam nec interdum tincidunt. Praesent et est et sem malesuada gravida. Suspendisse congue metus eu nisl lobortis dictum. Aenean dictum lacinia erat ac tincidunt. Nunc varius, est at lacinia semper, velit arcu convallis arcu, id ultrices sem erat at magna. Nunc in orci volutpat, porta quam congue, varius risus. Nulla placerat et magna sit amet consectetur. Suspendisse arcu leo, scelerisque a consectetur vel, vehicula nec tortor. Aenean tincidunt pulvinar semper. Proin ut eros eu tellus tempus feugiat.
+    </p>
+    <img src={image} alt="I made this"></img>
+  </div>
+  );
 }
 
 export default About;
